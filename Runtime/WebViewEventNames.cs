1b6c9fd91a62ebc61ec6399f43cdadb7ddb6ff99
Harrison <harrisonhough4@gmail.com>
2023-04-13 14:38:32 +0300

    chore: added missing namespace

--- Runtime/WebViewEventNames.cs
+++ Runtime/WebViewEventNames.cs
@@ -1,7 +1,10 @@
 
-public static class WebViewEventNames 
+namespace ReadyPlayerMe.WebView
 {
-    public const string AVATAR_EXPORT = "v1.avatar.exported";
-    public const string USER_SET = "v1.user.set";
-    public const string USER_AUTHORIZED = "v1.user.authorized";
+    public static class WebViewEventNames
+    {
+        public const string AVATAR_EXPORT = "v1.avatar.exported";
+        public const string USER_SET = "v1.user.set";
+        public const string USER_AUTHORIZED = "v1.user.authorized";
+    }
 }
