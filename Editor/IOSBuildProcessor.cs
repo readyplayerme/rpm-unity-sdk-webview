ff8c5b6dd8d36c37cc45c389dbfe725041367c28
Harrison Hough <harrisonhough4@gmail.com>
2024-01-09 09:55:35 +0200

    chore: readded ios post processor

---
+++ Editor/IOSBuildProcessor.cs
@@ -0,0 +1,24 @@
+#if UNITY_IOS
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Callbacks;
+
+namespace ReadyPlayerMe.WebView.Editor
+{
+    public class IOSBuildProcessor
+    {
+
+        [PostProcessBuildAttribute(100)]
+        public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+        {
+            if (BuildTarget.iOS != target) return;
+            var projectPath = $"{pathToBuiltProject}/Unity-iPhone.xcodeproj/project.pbxproj";
+            PBXProject proj = new PBXProject();
+            proj.ReadFromString(File.ReadAllText(projectPath));
+            proj.AddFrameworkToProject(proj.TargetGuidByName("Unity-iPhone"), "WebKit.framework", false);
+            File.WriteAllText(projectPath, proj.WriteToString());
+        }
+    }
+}
+#endif
